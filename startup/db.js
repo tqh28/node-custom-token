fc8e2f8157724471b51d769c4c589e98f39b4c4e
Huy Tran <tqh28>
2019-04-23 11:05:48 +0700

    add retry to connect to db

--- startup/db.js
+++ startup/db.js
@@ -2,10 +2,25 @@ const config = require('config');
 const mongoose = require('mongoose');
 const winston = require('winston');
 
-module.exports = () => {
+const connect = () => {
   const db = config.get('db');
-  mongoose.connect(db, { 'useNewUrlParser': true })
-    .then(() => winston.info(`Connected to ${db}...`))
-    .catch(() => winston.error(`Cannot connect to ${db}`));
+  mongoose.connect(db, {
+    useNewUrlParser: true,
+    server: {
+      // sets how many times to try reconnecting
+      reconnectTries: Number.MAX_VALUE,
+      // sets the delay between every retry (milliseconds)
+      reconnectInterval: 1000
+    }
+  })
+  .then(() => winston.info(`Connected to ${db}...`))
+  .catch(() => {
+    winston.error(`Cannot connect to ${db}`);
+    winston.info(`Retry to connect to ${db} after 10s`);
+    setTimeout(connect, 10000);
+  });
+  
   mongoose.set('useFindAndModify', false);
-}
+};
+
+module.exports = connect;
